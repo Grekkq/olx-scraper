9d5c507cd6c419649d28bcc3d7e5f554179f3508
Grzegorz Zagata <grzegorz.zagata2@ing.com>
2023-08-14 14:06:27 +0200

    make scrapping multiple pages possible

--- src/modules/Scraper.js
+++ src/modules/Scraper.js
@@ -2,52 +2,44 @@ const puppeteer = require('puppeteer');
 const Config = require('./Config');
 
 module.exports = async () => {
-    console.log("browser launch")
-    const browser = await puppeteer.launch({ headless: false, slowMo: 250 });
-    console.log("browser launched")
+    let options = {};
+    if (Config.debug) {
+        options = { ...options, headless: false, slowMo: 250 };
+    }
+    if (Config.chromiumPath?.length > 0) {
+        options = { ...options, executablePath: Config.chromiumPath };
+    }
+    const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
     await page.goto(Config.pageURL);
-    console.log("page loaded")
-    new Promise(r => setTimeout(r, 6000))
-    const fetchedOffers = await page.evaluate(() => {
-        const offers = Array.from(document.querySelectorAll(`[data-testid="location-date"]`));
-        const offersTitles = offers.map(offer => {
-            almostTitle = offer.parentElement.parentElement
-            url = almostTitle.parentElement.parentElement.parentElement.getAttribute('href')
-            if (url.startsWith("/d")) {
-                url = "https://www.olx.pl" + url
-            }
-            return {
-                title: almostTitle.firstElementChild.firstElementChild.innerText,
-                url: url,
-                price: almostTitle.firstElementChild.lastElementChild.innerText,
-                location: almostTitle.innerText
-            };
-        });
-        return offersTitles;
-    });
-    await page.goto(Config.pageURL.replace("?search", "?page=2"));
-    const anotherFetchedOffers = await page.evaluate(() => {
-        const offers = Array.from(document.querySelectorAll(`[data-testid="location-date"]`));
-        const offersTitles = offers.map(offer => {
-            almostTitle = offer.parentElement.parentElement
-            url = almostTitle.parentElement.parentElement.parentElement.getAttribute('href')
-            if (url.startsWith("/d")) {
-                url = "https://www.olx.pl" + url
-            }
-            return {
-                title: almostTitle.firstElementChild.firstElementChild.innerText,
-                url: url,
-                price: almostTitle.firstElementChild.lastElementChild.innerText,
-                location: almostTitle.innerText
-            };
-        });
-        return offersTitles;
-    });
-    console.log("offerts fetched")
-    var merged = [...fetchedOffers];
-    merged.concat(anotherFetchedOffers);
-    // console.log(merged)
+    let fetchedOffers = await extractOffers(page);
+    console.log(`page 1 scraped records: ${fetchedOffers.length}`);
+    for (let i = 2; i <= Config.numberOfPages; i++) {
+        await page.goto(Config.pageURL.replace("?search", `?page=${i}`));
+        const anotherFetchedOffers = await extractOffers(page);
+        fetchedOffers = fetchedOffers.concat(anotherFetchedOffers);
+        console.log(`page ${i} scraped, record: ${anotherFetchedOffers.length}`);
+    }
     await browser.close();
-    return merged;
+    return fetchedOffers;
 };
+
+async function extractOffers(page) {
+    new Promise(r => setTimeout(r, 6000))
+    return await page.evaluate(() => {
+        const offers = Array.from(document.querySelectorAll(`[data-testid="location-date"]`));
+        return offers.map(offer => {
+            let almostTitle = offer.parentElement.parentElement;
+            let url = almostTitle.parentElement.parentElement.parentElement.getAttribute('href');
+            if (url.startsWith("/d")) {
+                url = `https://www.olx.pl${url}`;
+            }
+            return {
+                title: almostTitle.firstElementChild.firstElementChild.innerText,
+                url: url,
+                price: almostTitle.firstElementChild.lastElementChild.innerText,
+                location: almostTitle.innerText
+            };
+        });
+    });
+}
